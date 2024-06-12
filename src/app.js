056d89ec4749ad97a4f8655fbefc7f24fa37f2d2
Dilan Pramodhya <dilanpramodhya44@gmail.com>
2024-06-12 19:17:05 +0530

    Server Configuration done

--- src/app.js
+++ src/app.js
@@ -1,1 +1,25 @@
-console.log("Hello You");
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import colors from "colors";
+
+const app = express();
+const PORT = process.env.PORT || "8090";
+
+app.use(cors());
+app.use(express.json({ limit: "20mb" }));
+
+app.get("/", (req, res, next) => {
+  res.send("<h2>📚 Library Management System 📚</h2>");
+  next();
+});
+
+app.listen(PORT, () => {
+  console.log(
+    "🚀" +
+      "  " +
+      `Server is up and running on PORT ${PORT}`.inverse +
+      " " +
+      "🔥"
+  );
+});
