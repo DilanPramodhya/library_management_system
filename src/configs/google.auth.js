8ee9cf88baf7c7c887cf2463a0a338e81abec8dd
Dilan Pramodhya <dilanpramodhya44@gmail.com>
2024-06-13 19:59:56 +0530

    User model created

--- src/configs/google.auth.js
+++ src/configs/google.auth.js
@@ -1,5 +1,6 @@
 import GoogleStrategy from "passport-google-oauth20";
 import config from ".";
+import User from "../api/model/user.model";
 
 const googleAuth = (passport) => {
   GoogleStrategy.Strategy;
@@ -13,9 +14,29 @@ const googleAuth = (passport) => {
         clientSecret: config.GOOGLE_CLIENT_SECRET,
         callbackURL: config.GOOGLE_REDIRECT_URL,
       },
-      (accessToken, refreshToken, profile, callback) => {
-        console.log(profile);
-        return callback(null, profile);
+      async (accessToken, refreshToken, profile, callback) => {
+        const userObj = {
+          googleId: profile.id,
+          displayName: profile.displayName,
+          gmail: profile.profile.email[0].value,
+          image: profile.photos[0].value,
+          firstName: profile.name.givenName,
+          lastName: profile.name.familyName,
+        };
+
+        // Select * from User WHERE googleId = profile.id
+        let user = await User.findOne({ googleId: profile.id });
+
+        if (user) {
+          return callback(null, user);
+        }
+        User.create({ userObj })
+          .then((user) => {
+            return callback(null, user);
+          })
+          .catch((err) => {
+            return callback(err.message);
+          });
       }
     )
   );
@@ -24,7 +45,9 @@ const googleAuth = (passport) => {
   });
 
   passport.deserializeUser((id, callback) => {
-    callback(null, id);
+    User.findById(id, (err, user) => {
+      callback(err, user);
+    });
   });
 };
 
